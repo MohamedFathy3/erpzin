4ce56ec7b2e3ca3adbd6880765c4d57f61fc2bfa
mohaemd <codehamada39@gmail.com>
2026-03-09 21:48:31 +0200

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 7000, 
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000",
+        target: "https://apierp.dentin.cloud",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, "/api"),
